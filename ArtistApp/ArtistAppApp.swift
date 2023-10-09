eb010eb3858b2f734f56a13b99457a104e0889b9
arwaOsaimi <arwaosaimi@hotmail.com>
2023-10-09 17:57:09 +0300

    Initial Commit

---
+++ ArtistApp/ArtistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistAppApp.swift
+//  ArtistApp
+//
+//  Created by Arwa Alosaimi on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ArtistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
